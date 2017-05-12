4cdf00fa1959d9670e7d073f85eb705749e060cc
冯尚实 <fengshangshi@fengshangshideMacBook-Pro.local>
2017-05-12 18:14:18 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # sugar-engine
+
+渲染模板使用，当前提供两种模板引擎：ejs和handlebars。
+
+用法如下：
+
+```
+const engine = require('sugar-engine');
+const render = engine('ejs');
+//render(template, data);
+```
--- libs/Engine.js
+++ libs/Engine.js
@@ -5,27 +5,18 @@
 'use strict'
 
 const ejs = require('ejs');
-const pug = require('pug');
 const hbs = require('handlebars');
 
 class Engine {
     constructor() {
         this.engines = {
             'ejs': this.ejsRender,
-            'pug': this.pugRender,
-            'jade': this.pugRender,
             'hbs': this.hbsRender,
-            'handlebars': this.hbsRender,
         }
     }
 
     ejsRender(source, data) {
         let template = ejs.compile(source);
-        return template(data);
-    }
-
-    pugRender(source, data) {
-        let template = pug.compile(source);
         return template(data);
     }
 
