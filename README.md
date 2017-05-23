51c9a483233794735f188291f3e06dbba14f0791
冯尚实 <fengshangshi@fengshangshideMacBook-Pro.local>
2017-05-23 19:59:13 +0800

    修改功能

--- README.md
+++ README.md
@@ -1,11 +1,69 @@
 # sugar-engine
 
-渲染模板使用，当前提供两种模板引擎：ejs和handlebars。
+渲染模板使用。当前提供两种模板引擎：ejs和handlebars。
 
-用法如下：
+
+## API
+### 引入
 
 ```
 const engine = require('sugar-engine');
-const render = engine('ejs');
-//render(template, data);
+
+```
+### engine.html(file, data, options)
+输入：模板文件、希望渲染的数据
+
+返回：编译后的html字符串
+
+* options.engine - 默认：ejs，指定模板渲染引擎
+* options.isCache - 默认：ture，模板文件是否被缓存
+
+```
+let data = {
+	username: 'sugar',
+	password: '123456',
+};
+
+let html = engine.html('/path/to', data, {
+	isCache: false,
+	engine: 'ejs', // or 'hbs'
+});
+```
+
+### engine.template(file, isCache)
+输入：模板文件，是否被缓存开关
+
+返回：模板字符串
+
+```
+let template = engine.template('/path/to', true);
+```
+
+### engine.render(engine)
+输入：模板引擎
+
+返回：模板渲染器
+```
+let render = engine.render('hbs');
+```
+
+### engine.ejs(source, data)
+输入：模板字符串，希望渲染的数据
+
+返回：按照ejs的规范编译后的字符串
+```
+let ejsHtml = engine.ejs('<h1><%=title%></h1>', {
+	title: 'sugar'
+});
+```
+
+
+### engine.hbs(source, data)
+输入：模板字符串，希望渲染的数据
+
+返回：按照handlebars的规范编译后的字符串
+```
+let hbsHtml = engine.ejs('<h1>{{title}}</h1>', {
+	title: 'sugar'
+});
 ```
--- index.js
+++ index.js
@@ -4,9 +4,53 @@
  */
 'use strict'
 
-const Engine = require('./libs/Engine');
-const e = new Engine;
+const fs = require('fs');
 
-module.exports = function(engine) {
-	return e.engine2render(engine);
+// 模板引擎
+const ejs = require('ejs');
+const hbs = require('handlebars');
+
+// sha1编码
+const sha1 = require('sha1');
+
+// 缓存
+const cache = require('sugar-utils').cache;
+
+exports.ejs = function(source, data) {
+	let template = ejs.compile(source);
+	return template(data);
+};
+
+exports.hbs = function(source, data) {
+	let template = hbs.compile(source);
+	return template(data);
+};
+
+exports.render = function(engine) {
+	return exports[engine];
+};
+
+exports.template = function(file, isCache) {
+	if (fs.existsSync(file) === false) {
+		return false;
+	}
+
+	let sign = sha1(file);
+
+	if (cache.has(sign)) {
+		return cache.get(sign);
+	}
+
+	let template = fs.readFileSync(file, {
+		encoding: 'utf8',
+	});
+
+	return isCache ? cache.set(sign, template).get(sign) : template;
+};
+
+exports.html = function(file, data, opts) {
+	let template = exports.template(file, opts.isCache);
+	let render = exports.render(opts.engine);
+
+	return render(template, data);
 };
